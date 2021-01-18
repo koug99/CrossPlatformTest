6faf187beed90abe833bf53a7f6a00cb2af86a31
KOUGHEN A L MOGAN <TP046367@mail.apu.edu.my>
2021-01-18 16:00:43 +0800

    barebones prototype

---
+++ CrossPlatformTest/DownloadPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System.IO;
+
+namespace CrossPlatformTest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DownloadPage : ContentPage
+    {
+        private static string dlURL;
+        public DownloadPage(string url)
+        {
+            dlURL = url;
+            InitializeComponent();
+
+            GenBarcode.BarcodeValue = dlURL;
+        }
+        
+    }
+}
---
+++ CrossPlatformTest/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Firebase.Storage;
+using System.Drawing;
+using System.IO;
+using System.Web;
+
+namespace CrossPlatformTest
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private static IEnumerable<FileResult> pickedFiles;
+        
+    
+        public MainPage()
+        {
+            InitializeComponent();
+            var deviceID = Preferences.Get("my_deviceID", string.Empty);
+            if (string.IsNullOrWhiteSpace(deviceID))
+            {
+                
+                deviceID = System.Guid.NewGuid().ToString();
+                Preferences.Set("my_deviceID", deviceID);
+            }
+        }
+
+
+        private async void ChooseFile_Pressed(object sender, EventArgs e)
+        {
+            pickedFiles = await FilePicker.PickMultipleAsync(new PickOptions
+            {
+                FileTypes = FilePickerFileType.Images,
+                PickerTitle = "Choose an image"
+            }) ;
+
+            if (pickedFiles != null)
+            {
+                foreach(var file in pickedFiles)
+                {
+                    file.ContentType = "application/x-www-form-urlencoded";
+                    lbl.Text += file.FileName;
+                }
+            }
+            else
+            {
+                await DisplayAlert("ERROR", "file not picked", "OK");
+            }
+        }
+        
+        
+
+        private async void UploadButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string dlURL = null;
+                foreach (var f in pickedFiles)
+                {
+                    var newstream = await f.OpenReadAsync();
+
+                    dlURL += await UploadFile(newstream, f.FileName) + "\n";
+                }
+
+                dlURL = HttpUtility.HtmlEncode(dlURL);
+                await DisplayAlert("Upload Successful!", dlURL , "Ok");
+                await Navigation.PushAsync(new DownloadPage(dlURL));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("ERROR", ex.ToString(), "Ok");
+            }
+            
+
+            
+        }
+        public async Task<string> UploadFile(Stream fileStream, string filename)
+        {
+
+            var fileURL = await new FirebaseStorage("universalfiletransfer.appspot.com")
+                .Child(Preferences.Get("my_deviceID", string.Empty))
+                .Child(filename)
+                .PutAsync(fileStream);
+
+            return fileURL;
+        }
+    }
+
+}
