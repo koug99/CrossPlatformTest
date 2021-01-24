fd7ab7b29f26f7a6f4dcb62774092f2391b2a925
KOUGHEN A L MOGAN <TP046367@mail.apu.edu.my>
2021-01-24 18:39:58 +0800

    segmented filetypes

--- CrossPlatformTest.Android/MainActivity.cs
+++ CrossPlatformTest.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Firebase;
 using Firebase.Storage;
 using System.Threading.Tasks;
 
--- CrossPlatformTest/MainPage.xaml.cs
+++ CrossPlatformTest/MainPage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Essentials;
-using Firebase.Storage;
+using Firebase;
+using Firebase.Storage; 
 using System.Drawing;
 using System.IO;
 using System.Web;
@@ -27,17 +28,29 @@ namespace CrossPlatformTest
                 deviceID = System.Guid.NewGuid().ToString();
                 Preferences.Set("my_deviceID", deviceID);
             }
+ 
         }
 
 
         private async void ChooseFile_Pressed(object sender, EventArgs e)
         {
+            FilePickerFileType fileType = null;
+
+
+            if(sender.Equals(img))
+            {
+                fileType = FilePickerFileType.Images;
+            }
+            else if(sender.Equals(vid))
+            {
+                fileType = FilePickerFileType.Videos;
+            }
 
             int index = 0;
             pickedFiles = await FilePicker.PickMultipleAsync(new PickOptions
             {
-                FileTypes = FilePickerFileType.Images,
-                PickerTitle = "Choose an image"
+                FileTypes = fileType,
+                PickerTitle = "Choose a file"
             }) ;
 
             if (pickedFiles != null)
@@ -71,22 +84,26 @@ namespace CrossPlatformTest
                     filepath.Add(f.FullPath);
                 }
 
-
-                ZipAndShip(filepath, cacheDir, zipPath);
-
-                FileStream fs = File.OpenRead(cacheDir +zipPath);
-
-                dlURL = await UploadFile(fs, zipPath);
-
-                fs.Close();
-
+                if (filepath.Count > 1)
+                {
+                    ZipAndShip(filepath, cacheDir, zipPath);
+                    FileStream fs = File.OpenRead(cacheDir + zipPath);
+                    dlURL = await UploadFile(fs, zipPath);
+                    fs.Close();
+                }
+                else
+                {
+                    FileStream fs = File.OpenRead(filepath[0]);
+                    dlURL = await UploadFile(fs, filepath[0].Substring(filepath[0].LastIndexOf("/") + 1));
+                    fs.Close();
+                }
                 dlURL = HttpUtility.HtmlEncode(dlURL);
-                await DisplayAlert("Upload Successful!", dlURL , "Ok");
+                await DisplayAlert("Upload Successful!", dlURL, "Ok");
                 await Navigation.PushAsync(new DownloadPage(dlURL));
-            }
+}
             catch (Exception ex)
             {
-                await DisplayAlert("ERROR", ex.ToString(), "Ok");
+                await DisplayAlert("No File Found", "Please select a file to upload", "Ok");
             }
             
 
@@ -94,7 +111,7 @@ namespace CrossPlatformTest
         }
         public async Task<string> UploadFile(Stream fileStream, string filename)
         {
-
+            
             var fileURL = await new FirebaseStorage("universalfiletransfer.appspot.com")
                 .Child(Preferences.Get("my_deviceID", string.Empty))
                 .Child(filename)
