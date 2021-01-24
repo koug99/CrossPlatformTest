0caa27078b79565fc94302ad47c700fe0ef17318
KOUGHEN A L MOGAN <TP046367@mail.apu.edu.my>
2021-01-25 04:26:14 +0800

    fixed missing app.xaml.cs

---
+++ CrossPlatformTest/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+
+namespace CrossPlatformTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var deviceID = Preferences.Get("my_deviceID", string.Empty);
+            if (string.IsNullOrWhiteSpace(deviceID))
+            {
+
+                deviceID = System.Guid.NewGuid().ToString();
+                Preferences.Set("my_deviceID", deviceID);
+            }
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            var cacheDir = FileSystem.CacheDirectory;
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
